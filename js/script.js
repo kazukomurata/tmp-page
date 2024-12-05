a6235cd87150894ed768c9a57ebde5303bffed61
kazukomurata <kazuko.murata@irologue.com>
2024-12-05 14:32:25 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const div = document.getElementById('myDiv');
 
 // 2. リンク要素を作成
 const link = document.createElement('a');
-link.href = '/test/'; // リンク先
+link.href = '/tmp-page/test/'; // リンク先
 link.textContent = 'Visit Test'; // 表示テキスト
 link.target = '_blank'; // 新しいタブで開く
 
